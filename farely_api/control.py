c5f8a19973a4442e7382901f2fc55da95b0b86fd
rtkleong10 <rtkleong10@gmail.com>
2020-03-11 16:05:52 +0800

    Revert "[fix] Fixed single trip fare type bug"
    This reverts commit addab9437c954ef5e8e7c18940b9be01177b01a9.

--- farely_api/control.py
+++ farely_api/control.py
@@ -147,13 +147,12 @@ class FareController():
 		return None
 
 	def calculateCashFare(self, steps):
-		fare_type = FareType.SINGLE_TRIP
 		total_fare = 0
 
 		for step in steps:
 			fare_category = step[0]
 			distance = step[1]
-			current_fare = self.getStepFare(fare_type, fare_category, distance)
+			current_fare = self.getStepFare(fare_category, distance)
 
 			if current_fare == None:
 				return None
