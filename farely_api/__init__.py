2fd90794d26bd1915af7567411b288d0cc31357a
rtkleong10 <rtkleong10@gmail.com>
2020-03-18 14:00:26 +0800

    [update] Fixed farely_api/__init__.py

--- farely_api/__init__.py
+++ farely_api/__init__.py
@@ -1,16 +1,5 @@
+"""Contains the API implementation for the farely server.
 """
-This module contains the API implementation for the farely server.
-"""
-
-__all__ = {
-	'apis',
-	'apps',
-	'boundary',
-	'control',
-	'entity',
-	'serializers',
-	'urls'
-}
 
 __pdoc__ = {
 	'migrations': False,
